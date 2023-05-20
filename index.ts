a9591a5d90ba739b33ad537e7eb0bc2f8cb9746f
CryingSadBaby <94829108+CryingSadBaby@users.noreply.github.com>
2023-05-20 08:44:57 +0000

    Init Koa server

--- index.ts
+++ index.ts
@@ -1,8 +1,52 @@
 import Koa from 'koa'
 import serve from 'koa-static'
+import logger from 'koa-logger'
+import json from 'koa-json'
+import cors from 'koa-cors'
+import passport from 'koa-passport'
+import Router, {RouterContext} from 'koa-router'
 
+//create server
 const app: Koa = new Koa()
+//create router
+const router: Router = new Router()
 
+//display welcome message function
+const welcomeMessage = async(ctx: RouterContext, next: object) => {
+  ctx.body = {message: 'Welcome to the TypeScript API!'}
+  await next()
+}
+
+const failedMessage = async( ctx: RouterContext, next: object) => {
+  try {
+    await next()
+    console.log(ctx.status)
+    if(ctx.status === 404) {
+      ctx.body = {err: 'Resource not found'}
+    }
+  } catch (err: err) {
+    ctx.body = {err: err}
+  }
+}
+
+//displayer welcome message on /api/v1 path
+router.get('/api/v1',welcomeMessage)
+
+//load function
+app.use(logger())
+app.use(json())
+
+//OpenAPI file
 app.use(serve('./docs'))
+
+//setup website communicate
+app.use(cors())
+app.use(passport.initialize())
+
+//Router routes
+app.use(router.routes())
+
+//When status 404
+app.use(failedMessage)
 
 app.listen(10888)
