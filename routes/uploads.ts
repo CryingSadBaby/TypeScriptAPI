608d4ead0ffba42f129e7dd291f650037a7ca253
CryingSadBaby <94829108+CryingSadBaby@users.noreply.github.com>
2023-06-11 13:47:44 +0000

    added upload image function

--- routes/uploads.ts
+++ routes/uploads.ts
@@ -1,62 +1,68 @@
 import { copyFileSync, existsSync, createReadStream} from 'fs'
-import koaBody from 'koa-body'
 import Router from 'koa-router'
 import {v4 as uuidv4} from 'uuid'
+import * as mime from 'mime-types'
+import koaBody from 'koa-body'
 
 const upload_options = {
   multipart: true,
   formidable: {uploadDir: './img'}
 }
-
-const fileStore='./img'
-
+const fileStore = './img'
 const prefix = '/api/v1'
 const router = Router({prefix: prefix})
 
-const upload = async (ctx) => {
+async function upload(ctx){
   try{
-    const {path, name, type} = ctx.request.files.upload
-    const extension = mime.extension(type)
-    console.log('Uploaded file details:')
-    console.log(`path: ${path}`)
-    console.log(`filename: ${name}`)
-    console.log(`type: ${type}`)
-    console.log(`extension: ${extension}`)
+    const {filepath,originalFilename,newFilename,mimetype} = ctx.request.files.upload
+    const extension = mime.extension(mimetype)
     const imageName = uuidv4()
     const newPath = `${fileStore}/${imageName}`
-    copyFileSync(path, newPath)
-    ctx.status=201
-    ctx.body={
-      filename: name,
-      type: type,
+    console.log('Uploaded file details:')
+    console.log(`path: ${filepath}`)
+    console.log(`filename: ${originalFilename}`)
+    console.log(`type: ${mimetype}`)
+    console.log(`extension: ${extension}`)
+    copyFileSync(filepath,newPath)
+    ctx.status = 201
+    ctx.body = {
+      filename: originalFilename,
+      type: mimetype,
       extension: extension,
-      links: {path: `https://${ctx.host}${router.url('get_image', imageName)}`}
+      links: {
+        path: `https://${ctx.host}${router.url('get_image', imageName)}`
+      }
     }
   } catch(error) {
     console.log(`error ${error.message}`)
-    ctx.throw(500,'upload error', {message: error.message})
+    ctx.throw(500, 'upload error', {message: error.message})
   }
 }
 
-// const getFile = async(ctx)=>{
-//   const uuid = ctx.params.uuid
-//   const path = `${fileStore}/${uuid}`
-//   console.log('client requested image with path',path)
-//   try{
-//     if(existsSync(path)){
-//       console.log('image found')
-//       const src = createReadStream(path)
-//       ctx.type = 'image/jpeg'
-//       ctx.body = src
-//       ctx.status = 200
-//     } else {
-//       console.log('image not found')
-//       ctx.status = 404
-//     }
-//   }
-// }
+async function getImage(ctx){
+  console.log('client requested image with path', path)
+  try{
+    if(existsSync(path)){
+      const uuid = ctx.params.uuid
+      const path = `${fileStore}/${uuid}`
+      console.log('image found')
+      const src = createReadStream(path)
+      ctx.type = 'image/jpeg'
+      ctx.body = src
+      ctx.status = 200
+    } else {
+      console.log('image not found')
+      ctx.status = 404
+    }
+  } catch (error) {
+    console.log(`error ${error.message}`)
+    ctx.throw(500, 'image download error', {message: error.message})
+  }
+}
 
-router.post('/images', koaBody, upload)
-//router.get('get_image','/images/:uuid([0-9a-f\\-](36))', getFile)
+
+router.post('/images', koaBody(upload_options), upload)
+router.get('get_image','/images/:uuid([0-9a-f\\-]{36})', getImage)
+
 
 export {router}
